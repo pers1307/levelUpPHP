342548c7cf574814b5092b984556d33d01e17b02
Yurii Pereskokov <skulines@mail.ru>
2016-02-01 17:57:58 +0500

    01.02.2016

--- classes.php
+++ classes.php
@@ -11,11 +11,37 @@
 
 class ShopProduct
 {
+    // Присваивание стандартных значений
+    public $title             = 'Стандартный товар';
+    public $producerMainName  = 'Фамилия автора';
+    public $producerFirstName = 'Имя автора';
+    public $price             = 0;
 
+    function __construct($title, $firstName, $mainName, $price)
+    {
+        $this->title             = $title;
+        $this->producerFirstName = $firstName;
+        $this->producerMainName  = $mainName;
+        $this->price             = $price;
+    }
+
+    function getProducer()
+    {
+        return $this->producerFirstName . ' ' . $this->producerMainName;
+    }
 }
 
 $product1 = new ShopProduct();
+print $product1->title;
+
 $product2 = new ShopProduct();
+$product2->title             = 'Собачье сердце';
+$product2->producerMainName  = 'Булгаков';
+$product2->producerFirstName = 'Михаил';
+$product2->price             = 5.99;
+echo '<br />';
+print 'Автор: ' . $product2->getProducer();
+
 
 var_dump($product1); // Рядом с объектов выводится его уникальный идентификатор
 var_dump($product2);
